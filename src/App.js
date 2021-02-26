3fbe78e9d64e99c5cabedcd65b5176ce4c7ebc79
sunan073 <msunan073@gmail.com>
2021-02-26 22:26:37 +0600

    :sparkles: Insert Event Into Calendar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import React, { Component } from "react";
 class App extends Component {
   state = {
     client: null,
+    GoogleAuth: null,
+  };
+
+  testEvent = {
+    summary: "Testing Event!",
+    location: "Mirpur, Dhaka",
+    description: "Testing my event",
+    start: {
+      dateTime: "2021-02-27T09:00:00-07:00",
+    },
+    end: {
+      dateTime: "2021-02-27T17:00:00-07:00",
+    },
+    // 'recurrence': [
+    //   'RRULE:FREQ=DAILY;COUNT=2'
+    // ],
+    attendees: [{ email: "xyz@gmail.com" }, { email: "abc@example.com" }],
+    reminders: {
+      useDefault: false,
+      overrides: [
+        { method: "email", minutes: 24 * 60 },
+        { method: "popup", minutes: 10 },
+      ],
+    },
   };
 
   gapi = window.gapi;
@@ -13,13 +37,11 @@ class App extends Component {
     "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
   ];
 
-  SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+  SCOPES = "https://www.googleapis.com/auth/calendar.events";
 
-  authorize = () => {
-    // console.log(this.gapi);
-    // if (!gapi) return;
+  componentDidMount() {
     this.gapi?.load("client:auth2", () => {
-      // console.log("loaded");
+      console.log("gapi loaded");
       // console.log(gapi.client.getToken);
       this.gapi?.client.init({
         apiKey: this.API_KEY,
@@ -27,18 +49,26 @@ class App extends Component {
         discoveryDocs: this.DISCOVERY_DOCS,
         scope: this.SCOPES,
       });
-      this.gapi?.client.load("calendar", "v3", () => console.log("calendar"));
-      this.gapi?.auth2
-        .getAuthInstance()
-        .signIn()
-        .then(() => {
-          this.setState({ client: this.gapi.client });
-        });
+    });
+  }
+
+  authorize = () => {
+    // console.log(this.gapi);
+    if (!this.gapi) return;
+    this.gapi?.client.load("calendar", "v3", () => console.log("calendar"));
+    var tempGauth = this.gapi?.auth2.getAuthInstance();
+
+    tempGauth.signIn().then(() => {
+      console.log({ ...this.gapi.client });
+      this.setState({ client: this.gapi.client, GoogleAuth: tempGauth });
     });
   };
 
   getData = () => {
     // console.log(this.state.client);
+    console.log(
+      this.state.GoogleAuth.currentUser.get().getBasicProfile().getEmail()
+    );
     this.state.client.calendar.events
       .list({
         calendarId: "primary", // can pass many params for query
@@ -48,10 +78,31 @@ class App extends Component {
       });
   };
 
+  insertEvent = () => {
+    var request = this.gapi.client.calendar.events.insert({
+      calendarId: "primary",
+      resource: this.testEvent,
+    });
+
+    request.execute((event) => {
+      console.log(event);
+      window.open(event.htmlLink);
+    });
+  };
+
   render() {
     return (
       <>
-        <div style={{ textAlign: "center" }}>
+        {/* Sign In Handle */}
+        <div style={{ textAlign: "center", fontSize: "2rem" }}>
+          {this.state.GoogleAuth !== null && (
+            <span>
+              {this.state.GoogleAuth.currentUser
+                .get()
+                .getBasicProfile()
+                .getEmail()}
+            </span>
+          )}
           <button
             onClick={this.authorize}
             style={{ fontSize: "2rem", margin: "3rem", padding: "1rem" }}
@@ -59,14 +110,33 @@ class App extends Component {
             Add Account
           </button>
         </div>
+
+        {/* Get Events From Calendar */}
         <div style={{ textAlign: "center" }}>
           <button
             disabled={this.state.client === null}
             onClick={this.getData}
-            style={{ fontSize: "2rem", margin: ".5rem", padding: "1rem" }}
+            style={{ fontSize: "2rem", margin: "1rem", padding: "1rem" }}
           >
             Get Data
           </button>
+        </div>
+
+        {/* Insert Event in Calendar */}
+        <div style={{ textAlign: "center" }}>
+          <button
+            disabled={this.state.client === null}
+            onClick={this.insertEvent}
+            style={{ fontSize: "2rem", margin: "1rem", padding: "1rem" }}
+          >
+            Insert Event
+          </button>
+        </div>
+
+        <div style={{ margin: "2rem", fontSize: "1.2rem" }}>
+          <b>Event details:</b>
+          <br />
+          {JSON.stringify(this.testEvent)}
         </div>
       </>
     );
